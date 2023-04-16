52416fa4c455fc23584b131e18cd0712f7d7f4a0
EPoY74 <127658418+EPoY74@users.noreply.github.com>
2023-04-16 10:45:40 +0500

    Рабочий и готовый бот. Иногда падает, т.к. не входных данных. Надо смотреть в телеграмме, юпочему так происходит.

---
+++ Bot_For_Working.py
@@ -0,0 +1,31 @@
+"""
+Этот бот, можно сказать, первый который что-то делает нужное.
+Считает сумму штрафа в день при 20% годовых. Требуется ввести сумму задолжности.
+Что бы не считать на калькуляторе.
+"""
+import telebot;
+
+bot = telebot.TeleBot('5416611054:AAFkdAkCTK5Kt8cWYJOb87w-QfDqzWxHKcE');
+
+@bot.message_handler(content_types=['text'])
+
+def get_text_messages(message):
+
+    if message.text == "/start" or message.text ==  "/help":
+        bot.send_message(message.from_user.id, "Считаю сумму штрафа в день при 20% годовых. Требуется ввести сумму задолжности.")
+
+    pers1 = message.text
+    if pers1.isdigit():
+        pers2 = float(pers1)
+        bot.send_message(message.from_user.id,'Штраф за просрочку платежа составляет ' + str(round(pers2 * (0.2 / 365),2)) + ' руб/день')
+    elif message.text != "/start":
+        bot.send_message(message.from_user.id, "Считаю сумму штрафа в день при 20% годовых. \nВведи число в рублях")
+
+#    if message.text == "Привет":
+#       bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+#    elif message.text == "/help":
+#        bot.send_message(message.from_user.id, "Напиши привет")
+#    else:
+#        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=0)
