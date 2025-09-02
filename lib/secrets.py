d8fb9094399d798f196301b8b9c015cac82790e3
Eugenii Petrov (asus550 wsl) <epoy74@gmail.com>
2025-09-02 11:27:48 +0500

    refactor: extract code to function get_bot_api_from_dotenv() with docstring and returned types

--- lib/secrets.py
+++ lib/secrets.py
@@ -10,21 +10,36 @@ import lib.project_log as proj_log
 
 logger = proj_log.logging.getLogger("lib/secrets.py")
 
-dotenv_path: str  = os.path.join(os.path.dirname(__file__),"..", '.env')
+def get_bot_api_from_dotenv() -> str | None:
+    """
+    Retrieve Telegram bot token from environment variables
+     loaded from .env file.
+    The .env file should be located in the root directory of the project
+    
+    Returns:
+        str: Bot token if found, None if not found or error occurs.
+    
+    Raises:
+        KeyError: If BOT_API_KEY not found in .env file
+    """
+        
+    dotenv_path: str  = os.path.join(os.path.dirname(__file__),"..", '.env')
 
-logger.info("---")
-logger.info(dotenv_path)
+    logger.info("---")
+    logger.info(dotenv_path)
 
-if not os.path.exists(dotenv_path):
-    logger.warning("File .env not found in current directory")
-    # raise
-elif not load_dotenv():
-    logger.warning("Environment variable(s) not found in .env file")
-    raise
-else:
-    if os.getenv('BOT_TOKEN'):
-        BOT_TOKEN = os.getenv('BOT_TOKEN')
-        logger.info("Bot token: %s", BOT_TOKEN)
-    else:
-        logger.error("Variable BOT_TOKEN not found")
+    if not os.path.exists(dotenv_path):
+        logger.warning("File .env not found in current directory")
+        # raise
+    elif not load_dotenv():
+        logger.warning("Environment variable(s) not found in .env file")
         raise
+    else:
+        if os.getenv('BOT_TOKEN'):
+            BOT_TOKEN = os.getenv('BOT_TOKEN')
+            logger.info("Bot token: %s", BOT_TOKEN)
+            return BOT_TOKEN
+        else:
+            logger.error("Variable BOT_TOKEN not found")
+            raise
+    return None
