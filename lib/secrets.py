421efab362a854ec107048e03f0ed197d841cd47
Eugenii Petrov (asus550 wsl) <epoy74@gmail.com>
2025-09-02 22:36:35 +0500

    refactoring: Formating by Ruff

--- lib/secrets.py
+++ lib/secrets.py
@@ -2,6 +2,7 @@
 Обработка и загрузка чувствительных данных из окружения
 Processing and Loading Sensitive Data from Environment Variables
 """
+
 import os
 
 from dotenv import load_dotenv
@@ -11,20 +12,21 @@ from lib.exceptions import EnvFileNotFound, RequiredEnvVariablesNotFound
 
 logger = proj_log.logging.getLogger("lib/secrets.py")
 
+
 def get_bot_api_from_dotenv() -> str | None:
     """
     Retrieve Telegram bot token from environment variables
      loaded from .env file.
     The .env file should be located in the root directory of the project
-    
+
     Returns:
         str: Bot token if found, None if not found or error occurs.
-    
+
     Raises:
         KeyError: If BOT_API_KEY not found in .env file
     """
-        
-    dotenv_path: str  = os.path.join(os.path.dirname(__file__),"..", '.env')
+
+    dotenv_path: str = os.path.join(os.path.dirname(__file__), "..", ".env")
 
     logger.info("---")
     logger.info(dotenv_path)
@@ -36,15 +38,16 @@ def get_bot_api_from_dotenv() -> str | None:
         logger.warning("Environment variable(s) not found in .env file")
         raise
     else:
-        required_vars: list[str]  = ["BOT_TOKEN"]
-        missing_vars  =  [var for var in required_vars if var not in os.environ]
+        required_vars: list[str] = ["BOT_TOKEN"]
+        missing_vars = [var for var in required_vars if var not in os.environ]
         if len(missing_vars) < 1:
-            BOT_TOKEN = os.getenv('BOT_TOKEN')
+            BOT_TOKEN = os.getenv("BOT_TOKEN")
             logger.info("Bot token: %s", BOT_TOKEN)
             return BOT_TOKEN
         else:
             logger.error("Variable BOT_TOKEN not found")
             raise RequiredEnvVariablesNotFound(missing_vars)
+
 
 if __name__ == "__main__":
     get_bot_api_from_dotenv()
